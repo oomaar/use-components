5be8f203c1be069a706bf46c20adb29baff7059b
Omar <omaar_5@hotmail.com>
2021-04-22 01:17:52 +0200

    style: use-globalstyle

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { GlobalStyle } from './GlobalStyle';
 
-render(<App />,document.getElementById('root'));
+render(
+    <>
+        <GlobalStyle />
+        <App />
+    </>
+    , document.getElementById('root'));
